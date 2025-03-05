629fc168b93ad9421d9f74ef6a09fb3a25e43ab3
kevinburger30 <75225138+kevinburger30@users.noreply.github.com>
2025-03-05 13:34:59 -0500

    Add files via upload

---
+++ src/pages/Connexion.js
@@ -0,0 +1,40 @@
+
+import React, { Component } from 'react';
+//import './App.css';
+
+class Connexion extends Component {
+    async Connexion(event) {
+        if (event) {
+            event.preventDefault(); 
+            var user=[document.getElementById("nom").value,document.getElementById("courriel").value, document.getElementById("password").value];
+            document.location.href="Magasiner";
+        }
+        }
+
+    render() { 
+        return <div>
+                <section id="back">
+                <a href="/" id="img"><img src="fleche.png"></img></a>
+        </section>
+        <h1>
+        Connexion
+        </h1>
+        <form onSubmit={this.Connexion}>
+            <input type="text" id="nom" placeholder="Nom"/>
+            <input type="text" id="courriel" placeholder="Courriel"/>
+            <input type="text" id="password" placeholder="Mot de passe"/>
+                        <button id="submit" color="primary" type="submit">Connexion</button>
+                </form>
+                <p id="two">
+
+        <a href="Creation">Pas de compte? Créer un compte.</a>
+        <a href="Oublier">Oublier le mot de passe</a>
+        <a href="Vendeur">Vendeur</a>
+        </p>
+
+
+</div>
+  }
+}
+ 
+export default Connexion;
