53270b128a58c20fb6251cd82fbc58e43e7a4ba4
kevinburger30 <75225138+kevinburger30@users.noreply.github.com>
2025-03-10 12:08:45 -0400

    Add files via upload

--- serveurjeux/app.js
+++ serveurjeux/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const mongodb = require("mongodb");
 const mongoose = require('mongoose');
 
 //routes et quelle port
@@ -23,19 +22,12 @@ const io = socketIo(server);
 
 let vendeur; //vendeur ou non
 let nonvalide; //compte pas creer et si vendeur
-let intervalReponse; //nombre de reponses
 
 let routeCreation; //route pour changer de creation a magasiner ou vendeur
 let routeConnexion; //route pour changer de creation a magasiner ou vendeur
 
 let trouver;
 let trouverFind;
-
-
-let listeBonne; //liste avec les bonnes repsonses
-let listeFausse; //liste avec les fausse repsonses
-let listeGagnant; //liste avec la page gagnante et linformation du joueur gagnant
-let listeTemps; //temps des utilisateurs
 
 var joueurs; //collection joueurs
 mongoose.connect('mongodb://127.0.0.1:27017/Prochevenement');
@@ -50,11 +42,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/Prochevenement');
   }));
 
 
-var questions; //collection questions
-questions = mongoose.model('questions', new mongoose.Schema({
+var evenements; //collection evenements 
+
+evenements = mongoose.model('evenements', new mongoose.Schema({
   _id: mongoose.Types.ObjectId,
-  question: String,
-  reponse: Boolean
+  nom: String,
+  description: String,
+  prix: Number,
+  date: String,
+  billets: Number
 }));
 
 io.on("connection", (socket) => {
@@ -70,6 +66,15 @@ io.on("connection", (socket) => {
       socket.emit("utilisateur", routeCreation, (callback)=>{
         vendeur=callback[4];
         create(callback);
+        
+      });
+
+
+      //creation dun evenement
+  
+
+      socket.emit("evenement", "/Vendeur", (callback)=>{
+        createEvent(callback);
         
       });
 
@@ -96,128 +101,31 @@ io.on("connection", (socket) => {
           
           }
         );
-/*
-      var promise=trouverQuestion("66eb1c42b3dd1ad002c4fc41");
-      var listeQuestionTrouver;
-      promise.then((data)=>{
-        listeQuestionTrouver=data;
-      
-    const messageQuestion=listeQuestionTrouver[0]; //question du serveur
-      socket.emit("connection", messageQuestion, (callbackMessage)=>{
-      });
 
-    let bonneReponse=listeQuestionTrouver[1]; //bonne reponse du serveur
-    if(intervalReponse>1){
-      if (listeBonne!=undefined){
-        listeTemps=creerTemps(listeBonne);
-        listeGagnant=["/gagnant", listeBonne[findIndex(listeTemps)],bonneReponse]; //message gagnant est un string avec le message qui contient le gagnant, la bonne reponse et le temps de reponse
-      }
-
-      else if (listeFausse!=undefined){
-        listeTemps=creerTemps(listeFausse);
-        listeGagnant=["/gagnant", listeFausse[findIndex(listeTemps)],bonneReponse]; //message gagnant est un string avec le message qui contient le gagnant, la bonne reponse et le temps de reponse
-      }
-    }
-      socket.emit("repondu", listeGagnant, (callbackRepondu)=>{
-        console.log(callbackRepondu); //mettre le temps [1] et la reponse [2] de l'utilisateur [0]
-        verifieReponse(callbackRepondu, bonneReponse);
-      });
-    })*/
 
 
 });
 
-function verifieReponse(callbackRepondu,bonneReponse){
-  if (intervalReponse==undefined){
-    intervalReponse=1;
-  }
-  else {
-    intervalReponse=intervalReponse+1;
-}
-  if (callbackRepondu[2]==bonneReponse){ //si utilisateur a la bonne reponse donc ajoute le a une liste de bonne reponses
-    if(listeBonne==undefined){
-      listeBonne=[callbackRepondu];
-    }
-    else{
-    listeBonne[intervalReponse-1]=callbackRepondu;
-    }
-  }
-  else{
-    if(listeFausse==undefined){
-      listeFausse=[callbackRepondu];
-    }
-    else{
-      listeFausse[intervalReponse-1]=callbackRepondu;
-  }
-}
-}
-
-function creerTemps(liste){
-  var n=0;
-  for (element in liste){
-    n++;
-  }
-  for(let i=0;i<n;i++){
-    if (i==0){
-      listeTemps=[liste[0][1]];
-    }
-    else{
-    listeTemps[i]=liste[i][1];
-    }
-  }
-      return listeTemps
-    }
-
-function findIndex(listeTemps){
-  var n=0;
-    var minTemps;
-    for (element in listeTemps){
-      n++;
-    }
-    for(let i=0;i<n;i++){
-      if (minTemps==undefined){
-        minTemps=listeTemps[i];
-      }
-      else if (minTemps>listeTemps[i]){
-        minTemps=listeTemps[i];
-      }
-    }
-  for(let i=0;i<n;i++){
-    if(minTemps==listeTemps[i]){
-      return i;
-    }
-  }
-}
-
-/*async function connection(){
-  return new Promise((resolve, reject) => {
-    mongoose.connect('mongodb://127.0.0.1:27017/Jeux');
-    if (joueurs==undefined){
-      joueurs = mongoose.model('joueurs', new mongoose.Schema({
-        _id: mongodb.ObjectId,
-        nom: String
-      }));
-      resolve(joueurs);
-    }
-    else{
-      resolve(joueurs);
-    }
-  })
-}*/
 
 async function create(user) {
-  //await connection();
   const nouveauJoueur = new joueurs({ _id: new mongoose.Types.ObjectId(), nom: user[0], courriel:user[1], postal:user[2], motpasse:user[3], vendeur:user[4]
  });
   await nouveauJoueur.save();
   console.log(nouveauJoueur.nom);
 }
 
+async function createEvent(event) {
+
+  const nouveauEvent = new evenements({ _id: new mongoose.Types.ObjectId(), nom: event[0], description:event[1], prix:event[2], date:event[3], billets:event[4]
+ });
+  await nouveauEvent.save();
+  console.log(nouveauEvent.nom);
+}
+
+
 async function trouverCompte(user){
 
-// find each person with a last name matching 'Ghost', selecting the `name` and `occupation` fields
 const joueur = await joueurs.findOne({ 'courriel': user[1] }, 'nom courriel postal motpasse vendeur');
-// Prints "Space Ghost is a talk show host".
 userFind=[joueur.nom, joueur.courriel, joueur.postal, joueur.motpasse, joueur.vendeur];
 if(user[0]==userFind[0] && user[1]==userFind[1] && user[2]==userFind[3]){
   return [true, userFind]
@@ -226,11 +134,6 @@ else{
   return [false, 0]
 }
 }
-
-/*
-async function 
-trouverQuestion et comparer a la reponse de l<utilisateur
-*/
 
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
