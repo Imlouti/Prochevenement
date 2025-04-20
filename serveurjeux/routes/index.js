7cb7d7148d22a2b09f2872046a5121b373f525b0
kevinburger30 <75225138+kevinburger30@users.noreply.github.com>
2025-04-20 13:44:04 -0400

    Delete serveurjeux directory

--- serveurjeux/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (res) => {
-  res.send({ response: "I am alive" }).status(200);
-}); //quand utilisateur se connecte il donne I am alive, pas socket
-
-module.exports = router;
