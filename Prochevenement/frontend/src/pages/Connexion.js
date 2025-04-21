6c796ae344383b269af64c0960a190f4c445284a
Caelan Bryk <Imlouti@users.noreply.github.com>
2025-04-21 10:01:59 -0400

    bump

--- Prochevenement/frontend/src/pages/Connexion.js
+++ Prochevenement/frontend/src/pages/Connexion.js
@@ -1,40 +1,191 @@
 
 import React, { Component } from 'react';
-import { Grid2 } from "@mui/material";
+<<<<<<< HEAD
 //import './App.css';
-
-import LoginComp from '../components/LoginComp';
+import { Paper, Grid2, Button, OutlinedInput, InputLabel, InputAdornment, IconButton, FormControl } from '@mui/material';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://192.168.37.1:4001";
 
 
-class Connexion extends Component {
-    async Submit(event) {
+function LoginComp() {
+
+    const [courriel, setCourriel] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const [nonExist, setNonExist] = React.useState(false);
+  
+    const [showPassword, setShowPassword] = React.useState(false);
+
+    const handleNonExisting = () => setNonExist((show) => !show);
+  
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+  
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+  
+    const handleMouseUpPassword = (event) => {
+      event.preventDefault();
+    };
+
+    const Submit = (event) => {
+
+        event.preventDefault();
 
         if (event) {
-            event.preventDefault(); 
-            document.location.href="Magasiner";
-                    
-        }
-    }
-
-    
-
-        
-    render() { 
-        return(
+            var user=courriel;
+            console.log(user);
+            var user2=[courriel, password];
+            if(user==''){
+                setNonExist(false);
+            }
             
-        <div id="background">
+            else{
+                user=user.split(",");
+                if(user[0]==user2[0] && user[1]==user2[1] && user[3]==user2[2]){
+                    if(user[4==1]){
+                        document.location.href="Vendeur";
+                    }
+                    else{
+                        document.location.href="Magasiner";
+                    }
+                }
+                else{
+                    document.getElementById("oublier").style.color="red";
+                }
+            }
+          
+        }
 
-        <section id="back">
-        <a href="/" id="img"><img src="fleche.png"></img></a>
-</section>
-<h1>
-Connexion
-</h1>
+    }
+  
+    return (
+      <Paper sx={{ textAlign: "center" }}>
+  
+        <Grid2
+          container
+          spacing={0}
+          direction="column"
+          sx={{backgroundColor: "transparent"}}
+          
+        >
+  
+        <FormControl sx={{ m: 1, width: '500px', alignSelf: 'center', backgroundColor: "transparent" }} variant="outlined">
+  
+          <InputLabel htmlFor="courriel">Couriel</InputLabel>
+          <OutlinedInput
+            id="courriel"
+            label="Courriel"
+            onInput={e=>setCourriel(e.target.value)}
+        />
+  
+        </FormControl>
+  
+        <FormControl sx={{ m: 1, width: '500px', alignSelf: 'center', backgroundColor: "transparent" }} variant="outlined">
+  
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <OutlinedInput
+              id="password"
+              type={showPassword ? 'text' : 'password'}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label={
+                      showPassword ? 'hide the password' : 'display the password'
+                    }
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    onMouseUp={handleMouseUpPassword}
+                    onInput={e => setPassword(e.target.value)}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
 
-<Grid2
+            
+  
+        </FormControl>
+        
+        <p
+        hidden={!nonExist}>Ce compte n'existe pas.</p>
+  
+            <Button 
+              label="Login" 
+              sx={{ 
+                m: 1,
+                alignSelf: 'center',
+                width:"25%" 
+              }}
+              variant="contained"
+              type="submit"
+              onClick={Submit}
+              >
+                Login
+            </Button>
+  
+        </Grid2>
+  
+      </Paper>
+    );
+  }
+
+class Connexion extends Component {
+        
+=======
+import { IconButton } from "@mui/material";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { Box, Link, Grid2 } from "@mui/material";
+import LoginComp from '../components/LoginComp';
+
+
+
+class Connexion extends Component {
+   
+        //Va afficher une fleche de retour a la page d'acceuil, le titre de la page, le formulaire de connexion (voire le fichier components/LoginComp pour plus d'explication) et deux liens pour creer un compte et pour le mot de passe oublier
+>>>>>>> 69fa7b0d5ef83c70fb546c6f4e82d6e5871a1d16
+    render() { 
+
+                return <div id="background">
+
+
+                <IconButton href="/" sx={{color:"black", padding: 0}} size="large">
+<ArrowBackIosIcon/>
+</IconButton>
+        <h1>
+        Connexion
+        </h1>
+<<<<<<< HEAD
+
+        <Grid2
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+        >
+            <LoginComp/>
+        </Grid2>
+
+        {//<form onSubmit={this.Submit}>
+            //<input type="text" id="courriel" class="input" placeholder="Courriel"/>
+            //<input type="text" id="password" class="input" placeholder="Mot de passe"/>
+                        //<button id="submit" class="button" color="primary" type="submit">Connexion</button>
+                        
+
+                //</form>
+                //<a id="hidden">Ce compte n'existe pas.</a>
+        }
+        <p id="two">
+        
+=======
+        <Grid2
   container
   spacing={0}
   direction="column"
@@ -43,26 +194,23 @@ Connexion
 >
     <LoginComp/>
 </Grid2>
-<form onSubmit={this.Submit}>
-    <input type="text" id="nom" class="input" placeholder="Nom"/>
-    <input type="text" id="courriel" class="input" placeholder="Courriel"/>
-    <input type="text" id="password" class="input" placeholder="Mot de passe"/>
-                <button id="submit" class="button" color="primary" type="submit">Connexion</button>
-                
 
-        </form>
-        <a id="hidden">Ce compte n'existe pas.</a>
-        <p id="two">
+                <a id="hidden">Ce compte n'existe pas.</a>
+                <a id="hidden2">Vous devez remplir tous les champs.</a>
+>>>>>>> 69fa7b0d5ef83c70fb546c6f4e82d6e5871a1d16
 
-<a href="Creation">Pas de compte? Créer un compte.</a>
-<a href="Oublier">Oublier le mot de passe</a>
 
-</p>
+    
+<Box sx={{display: 'flex', gap: 2}}>
+<Link href="Creation">Pas de compte? Créer un compte.</Link>
+<Link href="Oublier" id='oublier'>Oublier le mot de passe</Link>
+
+</Box>
 
 
 </div>
 
-    );
+    
 
   }
 
--- Prochevenement/frontend/src/pages/Creation.js
+++ Prochevenement/frontend/src/pages/Creation.js
@@ -1,18 +1,288 @@
 import React, { Component } from 'react';
+<<<<<<< HEAD
 //import './App.css';
-import { Grid2 } from '@mui/material';
-import socketIOClient from "socket.io-client";
-import RegisterComp from '../components/RegisterComp';
 
+import { FormControlLabel, Checkbox, Paper, Grid2, Button, OutlinedInput, InputLabel, InputAdornment, IconButton, FormControl } from '@mui/material';
+
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://192.168.37.1:4001";
+=======
+import { IconButton } from "@mui/material";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import {  Link, Grid2 } from "@mui/material";
+import RegisterComp from '../components/RegisterComp';
+>>>>>>> 69fa7b0d5ef83c70fb546c6f4e82d6e5871a1d16
+
+
+function RegisterComp() {
+
+    const [nom, setNom] = React.useState();
+    const [courriel, setCourriel] = React.useState();
+    const [postal, setPostal] = React.useState();
+    const [password, setPassword] = React.useState();
+    const [confPass, setConfPass] = React.useState();
+    const [vendor, setVendor] = React.useState(false);
+
+    const [passMatch, setPassMatch] = React.useState(true);
+
+    const [showPassword, setShowPassword] = React.useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = React.useState(false);
+
+    const handleClickShowConfirmPassword = () => setShowConfirmPassword((show) => !show);
+    const handleMouseDownConfirmPassword = (event) => {event.preventDefault();};
+    const handleMouseUpConfirmPassword = (event) => {event.preventDefault();};
+
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+    const handleMouseDownPassword = (event) => {event.preventDefault();};
+    const handleMouseUpPassword = (event) => {event.preventDefault();};
+
+    const handleVendorCheckbox = () => setVendor(!vendor);
+
+    const Submit = (event) => {
+        
+        if (event) {
+
+            event.preventDefault();
+
+            if (password == confPass && password != null) {
+
+                setPassMatch(true)
+
+                     
+                const socket = socketIOClient(ENDPOINT);
+                //client donne le surnom de l'utilisateur au serveur
+                var slider=2;
+
+                if(document.getElementById("slider").checked==true){
+                    slider=1;
+                }
+                else{
+                    slider=0;
+                }
+
+                var user=[nom, courriel, postal, password, vendor];
+                console.log(user);
+                var isAtLeastOneNull =user.some(function(i) { return i === ""; })
+                if(isAtLeastOneNull===true){
+                    document.getElementById("hidden").style.display="block";
+                }
+                else{
+                    socket.on("utilisateur", (arg, callback) => {
+                        callback(user);
+                    });
+                    //serbeur donne la route pour changer de page
+                    setInterval(() => {
+                        const socket = socketIOClient(ENDPOINT);
+                                socket.on("utilisateur", (arg, callback) => {
+                                    console.log(arg);
+                                    if(arg!=undefined){
+                                        localStorage.setItem("nom", arg[1]);
+                                        document.location.href=arg[0];
+                                    }
+                                });
+                    }, 1000);
+                }
+                
+
+            }
+
+            else {
+
+                setPassMatch(false)
+
+            }
+        }
+
+    }
+
+    return (
+        <Paper sx={{ textAlign: "center" }}>
+
+        <Grid2
+            container
+            spacing={0}
+            direction="column"
+            sx={{backgroundColor: "transparent"}}
+        >
+
+            <FormControl
+                sx={{
+                    m: 1,
+                    width: '500px',
+                    alignSelf: 'center',
+                    backgroundColor: "transparent"
+                }}
+                variant="outlined"
+                error={nom === ""}
+            >
+
+            <InputLabel htmlFor="nom">Nom*</InputLabel>
+            <OutlinedInput
+            id="nom"
+            label="nom"
+            required
+            onInput={e=> setNom(e.target.value)}
+            error={nom === ""}
+            
+            />
+
+            </FormControl>
+
+            <FormControl
+                sx={{
+                    m: 1, 
+                    width: '500px',
+                    alignSelf: 'center',
+                    backgroundColor: "transparent"
+                }}
+                variant="outlined"
+                error={courriel === ""}
+            >
+
+                <InputLabel htmlFor="email">Couriel*</InputLabel>
+                <OutlinedInput
+                id="email"
+                required
+                error={courriel === ""}
+                onInput={e => setCourriel(e.target.value)}
+                />
+
+            </FormControl>
+
+            <FormControl
+                sx={{
+                    m: 1,
+                    width: '500px',
+                    alignSelf: 'center',
+                    backgroundColor: "transparent"
+                }}
+                variant="outlined"
+                error={postal === ""}
+            >
+
+                <InputLabel htmlFor="postal">Code Postal*</InputLabel>
+                <OutlinedInput
+                id="postal"
+                required
+                inputProps={{ maxLength: 6}}
+                error={postal === ""}
+                onInput={e => setPostal(e.target.value)}
+                />
+
+            </FormControl>
+
+            <FormControl
+                sx={{
+                    m: 1,
+                    width: '500px',
+                    alignSelf: 'center',
+                    backgroundColor: "transparent"
+                }}
+                variant="outlined"
+                error={password === ""}
+            >
+
+                <InputLabel htmlFor="password">Mot de Passe*</InputLabel>
+                <OutlinedInput
+                    id="password"
+                    type={showPassword ? 'text' : 'password'}
+                    onInput={e => setPassword(e.target.value)}
+                    error={password === ""}
+                    endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton
+                        aria-label={
+                            showPassword ? 'hide the password' : 'display the password'
+                        }
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        onMouseUp={handleMouseUpPassword}
+                        edge="end"
+                        >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                    </InputAdornment>
+                    }
+                />
+
+            </FormControl>
+
+            <FormControl
+                sx={{
+                    m: 1,
+                    width: '500px',
+                    alignSelf: 'center',
+                    backgroundColor: "transparent"
+                }}
+                variant="outlined"
+                error={confPass === "" & confPass == password}
+            >
+
+                <InputLabel htmlFor="confirm-password">Confirme Mot de Passe*</InputLabel>
+                <OutlinedInput
+                    id="confirm-password"
+                    type={showConfirmPassword ? 'text' : 'password'}
+                    onInput={e => {
+                        setConfPass(e.target.value);
+                        
+                    }}
+                    endAdornment={
+                    <InputAdornment position="end">
+                        <IconButton
+                        aria-label={
+                            showConfirmPassword ? 'hide the password' : 'display the password'
+                        }
+                        onClick={handleClickShowConfirmPassword}
+                        onMouseDown={handleMouseDownConfirmPassword}
+                        onMouseUp={handleMouseUpConfirmPassword}
+                        edge="end"
+                        >
+                        {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                    </InputAdornment>
+                    }
+                    error={passMatch === false}
+                />
+
+            </FormControl>
+
+            <FormControl sx={{ m: 1, width: '500px', alignSelf: 'center', backgroundColor: "transparent" }}>
+
+                <FormControlLabel control={<Checkbox onChange={handleVendorCheckbox} />} label="Vendeur" />
+
+            </FormControl>
+
+            <Button 
+                sx={{ 
+                    m: 1,
+                    alignSelf: 'center',
+                    width:"25%" 
+                }}
+                variant="contained"
+                type='submit'
+                onClick={Submit}
+            >
+                Registre
+            </Button>
+
+        </Grid2>
+
+        </Paper>
+    );
+
+}
 
 
 class Creation extends Component {
+<<<<<<< HEAD
     async Submit(event) {
         if (event) {
             event.preventDefault(); 
             const socket = socketIOClient(ENDPOINT);
-            //client donne le surnom de l<utilisateur au serveur
+            //client donne le surnom de l'utilisateur au serveur
             var slider=2;
             if(document.getElementById("slider").checked==true){
                 slider=1;
@@ -26,34 +296,65 @@ class Creation extends Component {
             if(isAtLeastOneNull===true){
                 document.getElementById("hidden").style.display="block";
             }
+            
             else{
-            socket.on("utilisateur", (arg, callback) => {
-                callback(user);
-            });
-            //serbeur donne la route pour changer de page
-            setInterval(() => {
-                const socket = socketIOClient(ENDPOINT);
-                        socket.on("utilisateur", (arg, callback) => {
-                            console.log(arg);
-                            if(arg!=undefined){
-                                localStorage.setItem("nom", arg);
-                                document.location.href=arg;
-                            }
-                        });
-            }, 1000);
+                socket.on("utilisateur", (arg, callback) => {
+                    callback(user);
+                });
+                //serbeur donne la route pour changer de page
+                setInterval(() => {
+                    const socket = socketIOClient(ENDPOINT);
+                            socket.on("utilisateur", (arg, callback) => {
+                                console.log(arg);
+                                if(arg!=undefined){
+                                    localStorage.setItem("nom", arg[1]);
+                                    document.location.href=arg[0];
+                                }
+                            });
+                }, 1000);
+            }
         }
     }
-    }
+=======
+>>>>>>> 69fa7b0d5ef83c70fb546c6f4e82d6e5871a1d16
 
+        //Va afficher une fleche de retour a la page d'acceuil, le titre de la page, le formulaire de creation (voire le fichier components/RegisterComp pour plus d'explication) et un lien si le compte a deja ete creer
     render() { 
         return <div id="background">
-                <section id="back">
-        <a href="/" id="img"><img src="fleche.png"></img></a>
-        </section>
+                <IconButton href="/" sx={{color:"black", padding: 0}} size="large">
+<ArrowBackIosIcon/>
+</IconButton>
                 <h1>
         Creation d’un compte
         </h1>
+<<<<<<< HEAD
 
+        <Grid2
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+        >
+            <RegisterComp/>
+        </Grid2>
+
+        {/*<form onSubmit={this.Submit}>
+        <input type="text" id="nom" class='input' placeholder="Nom"/>
+            <input type="text" id="courriel" class='input' placeholder="Courriel"/>
+            <input type="text" id="postal" class='input' placeholder="Code postale"/>
+            <input type="text" id="password" class='input' placeholder="Mot de passe"/>
+                        <button id="submit" color="primary" type="submit" class='button'>Créer le compte</button>
+                        <div id="switch">
+            <label class="container">Vendeur
+  <input type="radio" id="slider"></input>
+  <span class="checkmark"></span>
+</label>
+
+</div>
+                </form>*/}
+        <a id="hidden">Vous devez remplir tous les champs.</a>
+=======
         <Grid2
         container
         spacing={0}
@@ -64,9 +365,11 @@ class Creation extends Component {
             <RegisterComp/>
         </Grid2>
 
-        <a id="hidden">Vous devez remplir tous les champs.</a>
+        <a id="hidden">Les mots de passe sont different.</a>
+        <a id="hidden2">Vous devez remplir tous les champs.</a>
+        <Link href="Connexion">Déja un compte? Connectez-vous.</Link>
+>>>>>>> 69fa7b0d5ef83c70fb546c6f4e82d6e5871a1d16
 
-        <a href="Connexion">Déja un compte? Connectez-vous.</a>
 
 </div>
   }
